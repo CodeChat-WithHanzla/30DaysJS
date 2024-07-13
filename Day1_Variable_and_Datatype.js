326774bceeea0e8d710b86f65b37af508757b5bc
CodeChat-WithHanzla <hanzlaasam@gmail.com>
2024-07-13 14:13:40 +0500

    Day 1

---
+++ Day1_Variable_and_Datatype.js
@@ -0,0 +1,41 @@
+// Activity 1: Variable Declaration
+// Task 1: Declare a variable using var, assign it a number, and log the value to the console.
+var a = 10;
+console.log(a);
+// Task 2: Declare a variable using let, assign it a string, and log the value to the console.
+let b = 'Hanzla';
+console.log(b);
+
+// Activity 2: Constant Declaration
+// Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
+const c = true;
+console.log(c);
+
+// Activity 3: Data Types
+// Task 4: Create variables of different data types (number, string, boolean, object, array) and log each variable's type using the typeof operator.
+let d = {}
+let e = []
+let f = ()=>{}
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+
+// Activity 4: Reassigning Variables
+// Task 5: Declare a variable using let, assign it an initial value, reassign a new value, and log both values to the console.
+let g = 20;
+g=30;
+console.log(g);
+
+// Activity 5: Understanding const
+// Task 6: Try reassigning a variable declared with const and observe the error.
+const h = 40;
+// h=50; Error Assignment to const variable
+
+/*
+Notes : 
+1_ Can`t reassign a value to const variable 
+2_ Can   reassign a value to let   variable but cant resign it by writing let again!
+*/
